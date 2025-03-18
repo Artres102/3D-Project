25eb0dc2e9be25f92e199a3549288a03335a5d25
Artres102 <150268118+Artres102@users.noreply.github.com>
2025-03-18 16:17:47 +0000

    More camera stuff

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 100f;
+    // Object related variables
+    private Transform cameraTransform;
+    private GameObject target;
+    
+    // Rotation related variables
+    [SerializeField] private float sensitivity;
     private float yRotation;
     private Vector3 rotate;
+    
+    // Position related variables
+    [SerializeField] private float yPosition = 3;
+    [SerializeField] private float zPosition = -5.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.FindWithTag("Player");
+        cameraTransform = transform.GetChild(0).transform;
+
+        if (target != null && cameraTransform != null)
+        {
+            Debug.Log(target.name);
+            cameraTransform.LookAt(target.transform.position);
+
+            cameraTransform.position = new Vector3(0, yPosition, zPosition);
+        }
+        sensitivity = PlayerPrefs.GetFloat("Sensitivity", 100f);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         
@@ -18,10 +39,12 @@ public class ThirdPersonCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        cameraTransform.LookAt(target.transform.position);
+        
         yRotation = Input.GetAxis("Mouse X");
         
         rotate = new Vector3(0,yRotation,0) * sensitivity * Time.deltaTime;
         
-        transform.eulerAngles += rotate;
+        transform.rotation *= Quaternion.Euler(rotate);
     }
 }
