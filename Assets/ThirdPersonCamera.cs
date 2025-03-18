e59e864ce1b2e2aadcfac18fac00a843f3caa7f3
Artres102 <150268118+Artres102@users.noreply.github.com>
2025-03-18 15:23:35 +0000

    Camera adjustments
    Changed camera behaviour entirely, removing the use of the Cinemachine package

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
+    [SerializeField] private float sensitivity = 100f;
+    private float yRotation;
+    private Vector3 rotate;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        yRotation = Input.GetAxis("Mouse X");
         
+        rotate = new Vector3(0,yRotation,0) * sensitivity * Time.deltaTime;
+        
+        transform.eulerAngles += rotate;
     }
 }
