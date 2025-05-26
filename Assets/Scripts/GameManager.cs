197a42609d39b7cc217f40ace16e52649fa3b6f1
Artres102 <150268118+Artres102@users.noreply.github.com>
2025-05-26 11:58:59 +0100

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : Singleton<GameManager>
     public GameObject interactionCanvas;
     public GameObject UI;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         camera = GameObject.FindGameObjectWithTag("MainCamera");
